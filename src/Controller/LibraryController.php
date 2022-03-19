a683512a4226a5cd4633b90e67d7604e3bba3f7a
carlosGomezMojica <carlos.gomez@luxitylab.com>
2022-03-19 13:43:54 +0100

    probando el routing

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController{
+
+    /*
+    *@Route("/library/list", name = "library_list")
+    */ 
+    public function list(){
+        $response = new Response();
+        $response -> setContent('<div>hola mundo</div>');
+        return $response;
+    }
+}
